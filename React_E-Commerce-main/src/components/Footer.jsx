41c4c92a16af0adf3876d5ad37171e04c5f8fa31
yamuna <chittiboyinayamuna@gmail.com>
2025-02-25 14:56:00 +0530

    first commit

---
+++ React_E-Commerce-main/src/components/Footer.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <>
+      <footer className="mb-0 text-center">
+        <div className="d-flex align-items-center justify-content-center pb-5">
+          <div className="col-md-6">
+            
+              <a  href="" className="text-decoration-underline text-dark fs-5" target="_blank" rel="noreferrer"></a>
+           
+            <a className="text-dark fs-4" href="" target="_blank" rel="noreferrer">
+              
+            </a>
+          </div>
+        </div>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
